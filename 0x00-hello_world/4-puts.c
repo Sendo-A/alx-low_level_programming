34307f9ccf4e7bef9ec335cfc6f80df291672d86
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-09-07 16:42:46 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * function that prints the sentence
+ * Retur: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
