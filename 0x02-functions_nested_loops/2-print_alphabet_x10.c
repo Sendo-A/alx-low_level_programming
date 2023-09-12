6add827368dd981fe74943d26a9392420efc3f65
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-09-12 12:52:32 -0700

    print 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print 10 times the alphabet
+ *
+ */
+void print_alphabet_x10(void);
+{
+	int row. let;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (let = 'a'; let <= 'z'; let++)
+			_putchar(let);
+		_putchar('\n');
+	}
+}
