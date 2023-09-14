1af9056337cab79a98155ae00d49609dd31e34c3
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-09-14 16:27:21 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * _isdigit - function that checks for a digit from 0 - 9
+ *
+ * @c: input as param
+ *
+ * Return: 1 if the cha is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if ( c >= 48 && <= 57)
+		return (1);
+	else
+		return (0);
+}
