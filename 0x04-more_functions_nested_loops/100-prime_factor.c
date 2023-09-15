52d9b545f8fd9236e9c7f2357ed7a7b57d6453cd
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-09-14 18:43:35 -0700

    function print prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints large prime factor
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i, k;
+
+	n = 612852475143;
+	for (k = 2; k <= i; k++)
+	{
+		if (i % k == 0)
+		{
+			i /= k;
+			k--;
+		}
+	}
+	printf("%ld\n", k);
+	return (0);
+}
