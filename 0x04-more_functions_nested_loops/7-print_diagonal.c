bddee4d54441612c47215df1047f46da337a78fc
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-09-14 17:50:57 -0700

    _putchar function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line on the terminal
+ *
+ * @n:is the number of times the \ character shoumd be printed
+ */
+void print_diagonal(int n)
+{
+	int ln, sp;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (ln = 1; ls <= n; ln++)
+		{
+			for (sp = 1; sp <= ln; sp++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
