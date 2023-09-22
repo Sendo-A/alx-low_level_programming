c17872da27b2e1b5e34f557cf1344a12efebbff9
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-09-21 17:31:26 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int c[5];
+	int *d;
+
+	c[2] = 1024;
+	d = &n;
+
+	*(d + 5) = 98;
+	printf("c[2] = %d\n", c[2]);
+	return (0);
+}
