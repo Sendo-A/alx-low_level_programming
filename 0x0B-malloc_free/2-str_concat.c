70d855a292d68567dccdd8c363894ce6ba9d5676
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-10-03 16:26:31 -0700

    function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * _strlen - find length of a string.
- * @s:string
- * Return: int
- */
+#include <stdlib.h>
+#include <string.h>
 
-int _strlen(char *s)
-{
-	int size = 0;
-
-	for (; s[size] != '\0'; size++)
-	;
-	return (size);
-}
 /**
  * *str_concat - function that concatenates two strings.
  * @s1: string 1
  * @s2: string 2
  * Return: pointer
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
-	char *m;
+	int i = 0, k = 0;
+	int size1 = 0, size2 = 0;
+	char *outcome;
 
-	if (s1 == NULL)
-		s1 = '\0';
-	if (s2 == NULL)
-		s2 = "\0";
+	if (s1 != NULL)
+		size1 = strlen(s1);
+	if (s2 != NULL)
+		size2 = strlen(s2);
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	m = malloc((size1 + size2) * sizeof(char) + 1);
-	if (m == 0)
+	outcome = malloc(size1 + size2 + 1)
+	if (outcome == 0)
 		return (0);
 
-	for (i = 0; i <= size1 + size2; i++)
-	{
-		if (i < size1)
-			m[i] = s1[i];
-		else
-			m[i] = s2[i - size1];
-	}
-	m[i] = '\0';
-	return (m);
+	for (i = 0; i < size1; i++)
+		outcome[i] = s1[i];
+	for (k = 0; k < size2; k++)
+		outcome[i + k] = s2[k];
+	outcome[i + k] = '\0';
+	return (outcome);
 }
