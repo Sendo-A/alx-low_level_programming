188f712cb6955f624b8b575c3a3d9ab05d627e96
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-10-07 17:59:34 -0700

    program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,10 @@
 #ifndef main_h
 #define main_h
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
+void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -11,6 +12,5 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
-void _puts(char *str);
 
 #endif /* main_h */
