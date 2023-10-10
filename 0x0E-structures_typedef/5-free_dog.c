77c32cd4079ac124bfc88542c4321ebf4d83367b
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-10-10 15:45:34 -0700

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs.
+ * @d: your dawg
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
