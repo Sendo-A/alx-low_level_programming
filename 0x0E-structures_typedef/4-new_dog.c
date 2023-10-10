c9b9dd33a3c7ff411dcd2346204b9cedc2c1c54a
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-10-10 15:37:21 -0700

    function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -63,7 +63,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	dog->name = _strcopy(dog->name, name);
-	dog->age = age
+	dog->age = age;
 	dog->owner =  _strcopy(dog->owner, owner);
 
 	return (dog);
