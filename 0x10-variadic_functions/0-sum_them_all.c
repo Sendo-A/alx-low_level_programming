69dfab7633d105fb421539620ccbfe280320187d
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-10-12 10:26:49 -0700

    function that splits a string into words.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (!n)
+	if (n == 0)
 		return (0);
 	va_start(ap, n);
-	while (i--)
+	while (i = 0; i < n; i++)
 		s += va_arg(ap, int);
 	va_end(ap);
 	return (s);
