48599f29f301ee568abab895da78c12afc5c3eef
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-10-11 17:29:59 -0700

    automatic functions implementation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+# include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: int
+ * @b: int
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two int
+ * @a: int
+ * @b: int
+ * Return: the difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two int
+ * @a: int
+ * @b: int
+ * Return: the product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/** op_div - divides two int
+ * @a: int
+ * @b: int
+ * Return: the quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b)
+}
+
+/** op_mod - module two int
+ * @a: int
+ * @b: int
+ * Return: the remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
