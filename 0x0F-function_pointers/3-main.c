a59b6496798fd5533a47fa9e2c0e1ef16e4f2b63
Abdelhadi <abdelhadi-chajia@outlook.fr>
2023-10-11 17:40:50 -0700

    automatic functions implementation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,10 +19,10 @@ int main(int argc, char **argv)
 
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
-		printf("Error\n"). exit(99);
+		printf("Error\n"), exit(99);
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"). exit(100);
+		printf("Error\n"), exit(100);
 
 	printf("%d\n", op_func(a, b));
 	return (0);
