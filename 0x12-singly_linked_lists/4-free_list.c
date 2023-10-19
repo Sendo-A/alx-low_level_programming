f0e0429dbc27e4f79712869ecf9d0101755d587a
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2023-10-19 15:38:06 -0700

    function that frees a list_t list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,36 +1,24 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a node to the end of the list
- * @head: address of pointer to head node
- * @str: str field of node
+ * free_list - frees all nodes of a list
+ * @head: pointer to head node
  *
- * Return: size of list
-*/
-list_t *add_node_end(list_t **head, const char *str)
+ * Return: void
+ */
+void free_list(list_t *head)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *node = *head;
+	list_t *node, *next_node;
+	
+	if (!head)
+		return;
 
-	if (!head || !new_node)
-		return (NULL);
-	if (str)
-	{
-		new_node->str = strdup(str);
-		if (!new_node->str)
+		node = head;
+		while (node)
 		{
-			free(new_node);
-			return (NULL);
+			next_node = node->next;
+			free(node->str);
+			free(node);
+			node = next_node;
 		}
-		new_node->len = _strlen(new_node->str);
-	}
-	if (node)
-	{
-		while (node->next)
-			node = node->next;
-		node->next = new_node;
-	}
-	else
-		*head = new_node;
-	return (new_node);
 }
