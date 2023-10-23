07b70fa43ea79f0dffeab5cd308c5938a2d7f7c0
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2023-10-23 14:32:49 -0700

    function that adds a new node at the beginning of a listint_t list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,13 +6,12 @@
  * Return: size of list
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
-	size_t i = 0;
+	size_t = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
