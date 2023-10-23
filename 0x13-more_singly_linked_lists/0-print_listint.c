c7c61525b86314cbffcb16e1e5858e27c1f00fb4
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2023-10-23 14:20:05 -0700

    unction that prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include <list.h>
+
+/**
+ * listint_len - prints a linked lists
+ * @h: pointer to first node
+ * Return: size of list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i)
+}
