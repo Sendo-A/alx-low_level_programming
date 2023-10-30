b702c5c203ed3349a8af14dd620af91a9605f93a
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2023-10-30 15:53:12 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	t = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSER | S_IWUSR);
+	t = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (t == -1)
 		return (-1);
 	if (len)
