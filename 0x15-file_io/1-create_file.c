73cc5eea6958f6ad7eed0a7b0f43bbe1bd4108e4
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2023-10-30 15:14:04 -0700

    Create a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: integer length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: text to write
+ * Return: 1 on success otherwise 0
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int t;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	t = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSER | S_IWUSR);
+	if (t == -1)
+		return (-1);
+	if (len)
+		bytes = write(t, text_content, len);
+	close(t);
+	return (bytes == len ? 1 : -1);
+}
