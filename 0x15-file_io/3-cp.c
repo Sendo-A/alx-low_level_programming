075300ce10fd4ad6292abc2149c0926a48d626de
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2023-10-30 17:08:44 -0700

    program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,8 +3,8 @@
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOEEAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
-#define ERR_NOCLOSE "Error: Can't close t %d\n"
-#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRB | S_IROTH)
+#define ERR_NOCLOSE "Error: Can't close td %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - program that copies the content of a file to another file.
@@ -15,31 +15,31 @@
 
 int main(int ac, char **av)
 {
-	int from_t = 0, to_t = 0;
+	int from_td = 0, to_td = 0;
 	ssize_t b;
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_t = open(av[1], O_RDONLY);
-	if (from_t == -1)
+	from_td = open(av[1], O_RDONLY);
+	if (from_td == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_t = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_t == -1)
+	to_td = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_td == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((b = read(from_t, buf, READ_BUF_SIZE)) > 0)
-		if (write(to_t, buf, b) != b)
+	while ((b = read(from_td, buf, READ_BUF_SIZE)) > 0)
+		if (write(to_td, buf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
-	from_t = close(from_t);
-	to_t = close(to_t);
-	if (from_t)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_t), exit(100);
-	if (to_t)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_t), exit(100);
+	from_td = close(from_td);
+	to_td = close(to_td);
+	if (from_td)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_td), exit(100);
+	if (to_td)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_td), exit(100);
 
 	return (EXIT_SUCCESS);
 }
